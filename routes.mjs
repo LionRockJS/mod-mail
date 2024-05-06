eff339b69c6464a27b1b4ea13cfea6e57fbfc941
Colin Leung <colin@cowise.co>
2024-05-07 00:51:50 +0800

    chore: replace KohanaJS

--- routes.mjs
+++ routes.mjs
@@ -1,11 +1,10 @@
-const { KohanaJS } = require('kohanajs');
-const { RouteList } = require('@kohanajs/mod-route');
+import { RouteList, Central } from '@lionrockjs/central';
 
-RouteList.add(`/webhook/${KohanaJS.config.mail.webhookKey}/:service/:type`, 'controller/Webhook', 'notification', 'POST');
+RouteList.add(`/webhook/${Central.config.mail.webhookKey}/:service/:type`, 'controller/Webhook', 'notification', 'POST');
 RouteList.add('/email/view/:id', 'controller/Email', 'view');
 
 RouteList.add('/unsubscribe/:id', 'controller/Email', 'unsubscribe');
-RouteList.add(`${KohanaJS.config.language?.route || '/:language'}/unsubscribe/:id`, 'controller/Email', 'unsubscribe');
+RouteList.add(`${Central.config.language?.route || '/:language'}/unsubscribe/:id`, 'controller/Email', 'unsubscribe');
 
 RouteList.add('/unsubscribe/thank-you', 'controller/Email', 'unsubscribe_thankyou');
-RouteList.add(`${KohanaJS.config.language?.route || '/:language'}/unsubscribe/thank-you`, 'controller/Email', 'unsubscribe_thankyou');
+RouteList.add(`${Central.config.language?.route || '/:language'}/unsubscribe/thank-you`, 'controller/Email', 'unsubscribe_thankyou');
